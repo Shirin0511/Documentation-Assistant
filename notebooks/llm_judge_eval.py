5c324670ba558398fc06f64371f650bb2c06ab61
Shirin Gupta <shirin.gupta05@gmail.com>
2026-02-17 01:42:38 +0530

    Evaluation checkes- Groundedness and Relevance Score

---
+++ notebooks/llm_judge_eval.py
@@ -0,0 +1,59 @@
+import os
+from langchain_groq import ChatGroq
+
+
+judge_llm= ChatGroq(
+    api_key= os.getenv("GROQ_API_KEY"),
+    model_name="llama-3.3-70b-versatile",
+    temperature=0,
+    max_tokens=200
+)
+
+
+def groundedness_score(question, context, answer):
+
+    prompt= """
+        You are aevaluating a RAG system.
+
+        Question:{question}
+
+        Context :
+        {context}
+
+        Answer :
+        {answer}
+
+        Is this answer fully supported by the context? 
+        Reply only in YES or NO
+
+    """
+
+    response= judge_llm.invoke(prompt).content.strip().lower()
+
+    if "YES" in response:
+        return 1
+    else:
+        return 0
+    
+
+
+
+def relevance_score(question, answer):
+
+    prompt= """
+
+    Question : {question}
+
+    Answer :
+    {answer}
+
+    Rate the relevance of the answer with respect to question on a scale of 1 to 5.
+    Only return number.
+
+    """
+
+    response = judge_llm.invoke(prompt).content.strip()
+
+    return int(response)
+
+
