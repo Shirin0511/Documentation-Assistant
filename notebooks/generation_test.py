94d269ea2a0826929df7216f022b30cabcff4672
Shirin Gupta <shirin.gupta05@gmail.com>
2026-03-24 23:30:49 +0530

    Updated generation file

--- notebooks/generation_test.py
+++ notebooks/generation_test.py
@@ -2,31 +2,32 @@ from RAG.vectorstore.ingestion import IngestionPipeline
 from RAG.generation.rag_chain import RAGGenerator
 import os
 
+
+pipeline = None
+generator = None
+
 def run_generation():
 
-    pipeline = IngestionPipeline(
+    if generator is None:
+        pipeline = IngestionPipeline(
         collection_name='github_issues_api_gen'
-    )
+        )
 
-    pipeline.ingest_url(
-        url='https://docs.github.com/en/rest/issues/issues',
-        api_name='github'
-    )
+        pipeline.ingest_url(
+            url='https://docs.github.com/en/rest/issues/issues',
+            api_name='github'
+        )
 
-    generator= RAGGenerator(
-                        vector_store= pipeline.vector_store,
-                        groq_api_key=os.getenv("GROQ_API_KEY")
-                        )
+        generator= RAGGenerator(
+                            vector_store= pipeline.vector_store,
+                            groq_api_key=os.getenv("GROQ_API_KEY")
+                            )
 
-    question = 'How do I create an issue using GitHub REST API?'
-
-    print(f"---QUESTION----{question}")
-
-    answer= generator.generate(question)
-
-    print(f"-----ANSWER-------{answer}")
+        return generator
 
 
+def generate_answer(question):
 
-if __name__ == "__main__":
-    run_generation()
+    gen = run_generation()
+    return gen.generate(question)
+
